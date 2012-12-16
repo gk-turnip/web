1193763dcf65c724146ea690ce6ec0e518bcb97d
Neil Thiessen <neil.w.thiessen@gmail.com>
2012-12-16 13:33:40 -0600

    Migrate all pages to a simple PHP include system.

---
+++ main/game_starter.php
@@ -0,0 +1,19 @@
+<?php include "includes/header.php" ?>
+
+<h1>Starter Game Document</h1>
+<p>
+  <ul>
+    <li>The smallest possible game needs to be created first
+      <ul>
+        <li>it must demonstrate basic game play</li>
+        <li>it must prove the chosen technologies</li>
+        <li>it can be used as a demonstration</li>
+        <li>it can be built upon to make a full game</li>
+      </ul>
+    </li>
+  </ul>
+</p>
+<p class="contributors"></p>
+<p class="updated"></p>
+
+<?php include "includes/footer.php" ?>
