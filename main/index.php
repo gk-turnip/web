1f5135ede042d01d810fc93db2934da802ae7a95
diver2 <diver2@mycroft.oakark.com>
2013-02-07 19:15:33 -0500

    add link to game server, add to_do.

--- main/index.php
+++ main/index.php
@@ -3,6 +3,10 @@
 <?php include "includes/header.php" ?>
 
 <div class="about">What is Gourdian Knot? It is a community-based, community-built, cooperative offbeat fantasy MMO.  We are still in the very early stages so have a look around, join the forum, add your suggestions, help set the direction of Gourdian Knot.</div>
+<br/>
+<div class="about">
+Link to the <a href="https://gourdianknot.com/gk/loginServer">Game Server</a>.
+</div>
 
 <div id="blog-posts">Getting latest blog posts…</div>
 
