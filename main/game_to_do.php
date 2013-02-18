04dc3c4e350f0c7d6df928f471e5f9454f57a633
turnip <turnip@gourdianknot.com>
2013-02-18 17:41:19 -0500

    minor to_do update

--- main/game_to_do.php
+++ main/game_to_do.php
@@ -8,16 +8,10 @@ This is a breif short term to do list, mostly for myself to keep track of what I
 <p>Turnip</p>
 <br/>
 <ul>
-<li>get two avatars at once working</li>
-<li>new routine to traverse all small diamond iso positions</li>
-<li>new routine to traverse all large diamond iso positions</li>
-<li>new routine to convert between windows and iso large diamond positions</li>
-<li>draw large diamond iso mono colour</li>
-<li>draw large diamond iso from svg</li>
 <li>background sound does not loop</li>
 <li>add copyright text to all javascript</li>
 <li>check if rain causes lag, put rain and avatar movement in same loop</li>
-<li>select "--select avatar--" causes problem, avatar disapears</li>
+<li>refine movement of other player avatars</li>
 <li>look into requirejs</li>
 <li>look into wss:// instead of ws:// for websockets</li>
 </ul>
