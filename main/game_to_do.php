3eaafb69b57e47fbd27f6cd3a2ba5dd9f711f2ca
turnip <turnip@gourdianknot.com>
2013-03-03 16:56:05 -0500

    minor to_do update

--- main/game_to_do.php
+++ main/game_to_do.php
@@ -8,20 +8,16 @@ This is a breif short term to do list, mostly for myself to keep track of what I
 <p>Turnip</p>
 <br/>
 <ul>
-<li>add copyright text to all javascript *DONE*</li>
 <li>check if rain causes lag, put rain and avatar movement in same loop</li>
-<li>refine movement of other player avatars *DONE*</li>
 <li>look into requirejs</li>
 <li>look into issue tracking system (bugzilla http://www.bugzilla.org,mantis http://www.mantisbt.org)</li>
-<li>add "chat" within the game server</li>
-<li>update the build documentation, wws, new config.xml stuff</li>
-<li>refactor the use of c_rand and m_rand into gkrand package</li>
 <li>current list of bugs:</li>
 <li><ul>
 <li>When you change avatars, the avatar still moves back to home position instead of remaining where it was when you clicked to change.</li>
-<li>There are still dandelions periodically showing up with black foliage instead of green. *DONE*</li>
-<li>Avatars still sometimes switch to black and white in Chrome and Chromium. create a test that does much higher rate if switch in/out of the dandelion, change dandelion colouring to determine what is causing this. *DONE*</li>
-<li>if you switch the avatar enough times, the game will stop responding *DONE*</li>
+<li>chat needs the message and user name "json escaped"</li>
+<li>fix server when websocket is trying to re-connect</li>
+<li>re-direct on invalid token is not correct</li>
+<li>chat needs the message cleared once sent</li>
 </ul></li>
 
 </ul>
